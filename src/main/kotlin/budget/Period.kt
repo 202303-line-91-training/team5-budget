41a4457ebff463205cceec89a5dbf29a8675ce42
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 00:50:38 +0800

    clean up

--- src/main/kotlin/budget/Period.kt
+++ src/main/kotlin/budget/Period.kt
@@ -1,24 +1,13 @@
 package budget
 
 import java.time.LocalDate
-import java.time.YearMonth
 import java.time.temporal.ChronoUnit
 
-class Period(val startDate: LocalDate, val endDate: LocalDate) {
+class Period(private val startDate: LocalDate, private val endDate: LocalDate) {
 
     fun overlappingDays(budget: Budget): Long {
         val overlappingStart: LocalDate = if (startDate.isAfter(budget.firstDay())) startDate else budget.firstDay()
         val overlappingEnd: LocalDate = if (endDate.isBefore(budget.lastDay())) endDate else budget.lastDay()
-        if (budget.getYearMonth() == YearMonth.from(startDate)) {
-//            overlappingEnd = budget.lastDay()
-//            overlappingStart = startDate
-        } else if (budget.getYearMonth() == YearMonth.from(endDate)) {
-//            overlappingEnd = endDate
-//            overlappingStart = budget.firstDay()
-        } else {
-//            overlappingEnd = budget.lastDay()
-//            overlappingStart = budget.firstDay()
-        }
         return ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
     }
 }
