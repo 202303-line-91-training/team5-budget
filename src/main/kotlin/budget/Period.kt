50e3afc673f04a8a2f0abbc4cdee9b190115c2e7
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 00:50:09 +0800

    use period domain logic for overlappingEnd

--- src/main/kotlin/budget/Period.kt
+++ src/main/kotlin/budget/Period.kt
@@ -8,15 +8,15 @@ class Period(val startDate: LocalDate, val endDate: LocalDate) {
 
     fun overlappingDays(budget: Budget): Long {
         val overlappingStart: LocalDate = if (startDate.isAfter(budget.firstDay())) startDate else budget.firstDay()
-        val overlappingEnd: LocalDate
+        val overlappingEnd: LocalDate = if (endDate.isBefore(budget.lastDay())) endDate else budget.lastDay()
         if (budget.getYearMonth() == YearMonth.from(startDate)) {
-            overlappingEnd = budget.lastDay()
+//            overlappingEnd = budget.lastDay()
 //            overlappingStart = startDate
         } else if (budget.getYearMonth() == YearMonth.from(endDate)) {
-            overlappingEnd = endDate
+//            overlappingEnd = endDate
 //            overlappingStart = budget.firstDay()
         } else {
-            overlappingEnd = budget.lastDay()
+//            overlappingEnd = budget.lastDay()
 //            overlappingStart = budget.firstDay()
         }
         return ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
