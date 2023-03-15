c3525ec80dca4ff41b76b7c25ee15ff546910461
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 00:52:30 +0800

    use another for firstDay and lastDay

--- src/main/kotlin/budget/Period.kt
+++ src/main/kotlin/budget/Period.kt
@@ -7,8 +7,8 @@ class Period(private val startDate: LocalDate, private val endDate: LocalDate) {
 
     fun overlappingDays(budget: Budget): Long {
         val another = Period(budget.firstDay(), budget.lastDay())
-        val firstDay = budget.firstDay()
-        val lastDay = budget.lastDay()
+        val firstDay = another.startDate
+        val lastDay = another.endDate
         val overlappingStart: LocalDate = if (startDate.isAfter(firstDay)) startDate else firstDay
         val overlappingEnd: LocalDate = if (endDate.isBefore(lastDay)) endDate else lastDay
         return ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
