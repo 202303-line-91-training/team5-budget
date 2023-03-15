e8f111aaf9912ee04846212ef2727f27829ab177
Joey Chen <jay.hatelove@gmail.com>
2023-03-15 23:59:37 +0800

    use YearMonth.parse() with DateTimeFormatter.ofPattern("yyyyMM")

--- src/main/kotlin/budget/Budget.kt
+++ src/main/kotlin/budget/Budget.kt
@@ -1,10 +1,7 @@
 package budget
 
-import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
-import java.util.Locale
 
 data class Budget(
     val yearMonth: String = "",
@@ -12,21 +9,11 @@ data class Budget(
 ) {
 
     fun dailyAmount(): Double {
-        return amount.toDouble() / YearMonth.of(getYearMonth().year, getYearMonth().monthValue)
-            .lengthOfMonth()
-    }
-
-    fun getYearMonthDate(): LocalDate {
-        return getYearMonth().atDay(1)
+        return amount.toDouble() / getYearMonth().lengthOfMonth()
     }
 
     fun getYearMonth(): YearMonth {
-        val formatter: DateTimeFormatter = DateTimeFormatterBuilder()
-            .parseCaseInsensitive()
-            .appendPattern("uuuuMM")
-            .toFormatter(Locale.ENGLISH)
-
-        return YearMonth.parse(yearMonth, formatter)
+        return YearMonth.parse(yearMonth, DateTimeFormatter.ofPattern("yyyyMM"));
     }
 
 }
