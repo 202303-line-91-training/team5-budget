4ec8b2e277e985c77ef07219d50d783c0ec3e21b
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 00:49:20 +0800

    use period domain logic for overlappingStart

--- src/main/kotlin/budget/Period.kt
+++ src/main/kotlin/budget/Period.kt
@@ -7,17 +7,17 @@ import java.time.temporal.ChronoUnit
 class Period(val startDate: LocalDate, val endDate: LocalDate) {
 
     fun overlappingDays(budget: Budget): Long {
-        val overlappingStart: LocalDate
+        val overlappingStart: LocalDate = if (startDate.isAfter(budget.firstDay())) startDate else budget.firstDay()
         val overlappingEnd: LocalDate
         if (budget.getYearMonth() == YearMonth.from(startDate)) {
             overlappingEnd = budget.lastDay()
-            overlappingStart = startDate
+//            overlappingStart = startDate
         } else if (budget.getYearMonth() == YearMonth.from(endDate)) {
             overlappingEnd = endDate
-            overlappingStart = budget.firstDay()
+//            overlappingStart = budget.firstDay()
         } else {
             overlappingEnd = budget.lastDay()
-            overlappingStart = budget.firstDay()
+//            overlappingStart = budget.firstDay()
         }
         return ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
     }
