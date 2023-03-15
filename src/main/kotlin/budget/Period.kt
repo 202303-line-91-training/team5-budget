760dcaa681379449af81a36f1841852e8edd96e9
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 00:52:49 +0800

    inline temp variables: firstDay and lastDay

--- src/main/kotlin/budget/Period.kt
+++ src/main/kotlin/budget/Period.kt
@@ -7,10 +7,8 @@ class Period(private val startDate: LocalDate, private val endDate: LocalDate) {
 
     fun overlappingDays(budget: Budget): Long {
         val another = Period(budget.firstDay(), budget.lastDay())
-        val firstDay = another.startDate
-        val lastDay = another.endDate
-        val overlappingStart: LocalDate = if (startDate.isAfter(firstDay)) startDate else firstDay
-        val overlappingEnd: LocalDate = if (endDate.isBefore(lastDay)) endDate else lastDay
+        val overlappingStart: LocalDate = if (startDate.isAfter(another.startDate)) startDate else another.startDate
+        val overlappingEnd: LocalDate = if (endDate.isBefore(another.endDate)) endDate else another.endDate
         return ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
     }
 }
