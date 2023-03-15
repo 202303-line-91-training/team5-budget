31958c2cad18cec0de8d440fbb90a237f29ab287
Joey Chen <jay.hatelove@gmail.com>
2023-03-16 01:01:42 +0800

    replace with sumByDouble()

--- src/main/kotlin/budget/BudgetService.kt
+++ src/main/kotlin/budget/BudgetService.kt
@@ -1,8 +1,6 @@
 package budget
 
 import java.time.LocalDate
-import java.time.YearMonth
-import java.time.temporal.ChronoUnit
 
 class BudgetService(
     private val budgetRepo: BudgetRepo
@@ -10,24 +8,9 @@ class BudgetService(
 
     fun query(startDate: LocalDate, endDate: LocalDate): Double {
         if (endDate.isBefore(startDate)) return 0.0
-        val budgets = budgetRepo.getAll()
 
-//        var currentYearMonth = YearMonth.from(startDate)
-        var amount = 0.00
-//        if (YearMonth.from(startDate) == YearMonth.from(endDate)) {
-//            val overlappingStart = startDate
-//            val overlappingEnd = endDate
-//            val overlappingDays = ChronoUnit.DAYS.between(overlappingStart, overlappingEnd) + 1
-//            val budget = budgets.find { it.getYearMonth() == currentYearMonth }
-//            if (budget != null) {
-//                return budget.dailyAmount() * overlappingDays
-//            }
-//        }
         val period = Period(startDate, endDate)
-        for (budget in budgets) {
-            amount += budget.overlappingAmount(period)
-        }
-        return amount
+        return budgetRepo.getAll().sumByDouble { it.overlappingAmount(period) }
     }
 
 }
